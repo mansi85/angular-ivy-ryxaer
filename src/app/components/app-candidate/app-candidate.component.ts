9991e4f84c87899fa1047a93ac66944277f38b2a
mansi85 <bhatia.mansi12@gmail.com>
2021-09-08 19:33:31 +0530

    2 files modified

--- src/app/components/app-candidate/app-candidate.component.ts
+++ src/app/components/app-candidate/app-candidate.component.ts
@@ -10,19 +10,50 @@ import { CandidateService } from '../../services/candidate.service';
 export class AppCandidateComponent {
   female = false;
   male = false;
-  candidatesList;
+  candidatesList = [
+    {
+      id: 1,
+      first_name: 'Jeanette',
+      last_name: 'Penddreth',
+      email: 'jpenddreth0@census.gov',
+      gender: 'Female'
+    },
+    {
+      id: 2,
+      first_name: 'Giavani',
+      last_name: 'Frediani',
+      email: 'gfrediani1@senate.gov',
+      gender: 'Male'
+    },
+    {
+      id: 3,
+      first_name: 'Noell',
+      last_name: 'Bea',
+      email: 'nbea2@imageshack.us',
+      gender: 'Female'
+    },
+    {
+      id: 4,
+      first_name: 'Willard',
+      last_name: 'Valek',
+      email: 'wvalek3@vk.com',
+      gender: 'Male'
+    }
+  ];
   searchTerm = '';
 
   constructor(private candidateService: CandidateService) {}
 
   ngOnInit() {
-    this.candidateService.getCandidatesList().subscribe(data => {
-      this.candidatesList = JSON.stringify(data);
-      console.log(data);
-    });
+    //data was not coming. Hence, hardcoded above. something with stackblitz since http get looks good to me
+    // this.candidateService.getCandidatesList().subscribe(data => {
+    //   this.candidatesList = JSON.stringify(data);
+    //   console.log(data);
+    // });
   }
 
   filterCandidateList() {
+    console.log('in search');
     this.candidatesList.filter(
       candidate =>
         candidate['firstNAme'].includes(this.searchTerm) ||
